650e609d40197bde3fbaaeed73a1721578e4662e
agungTuanany <agung.tuanany@gmail.com>
2020-06-02 00:02:08 +0700

    Simple Bulma mockup implementation

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,1 +1,35 @@
-<h1>Hello Cio !</h1>
+<script charset="utf-8">
+    // Declared variable
+    let input = 0
+    let typeOfTransaction = "+"
+</script>
+
+<style type="text/css" media="all">
+    .app {
+        margin      : 40px auto;
+        max-width   : 500px;
+    }
+</style>
+
+<div class="app container">
+    <div class="field has-addons">
+        <p class="control">
+            <span class="select">
+                <select bind:value={typeOfTransaction}>
+                    <option value="+">+</option>
+                    <option value="-">-</option>
+                </select>
+            </span>
+        </p>
+        <p class="control is-expanded">
+            <input class="input" type="number" bind:value={input} placeholder="Amount of money">
+        </p>
+        <p class="control">
+            <button class="button">
+                Save
+            </button>
+        </p>
+    </div>
+    <p>{input}</p>
+    <h1>{typeOfTransaction}</h1>
+</div>
