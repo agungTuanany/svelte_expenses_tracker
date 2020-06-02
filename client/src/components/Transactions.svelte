cbb569755ec2a30a5b814d64eba47489fac9c399
agungTuanany <agung.tuanany@gmail.com>
2020-06-02 13:13:55 +0700

    Add date in each transactions

--- client/src/components/Transactions.svelte
+++ client/src/components/Transactions.svelte
@@ -10,7 +10,17 @@
     export let removeTransaction
 </script>
 
+<style>
+    .date {
+        float   : right;
+    }
+
+</style>
+
 <div class="notification is-light {transaction.value > 0 ? "is-success" : "is-danger"}" transition:slide={{ duration: 1000 }}>
     {transaction.value}
+    <span class="is-size-7 has-text-grey date">
+        {new Date (transaction.date).toLocaleString ()}
+    </span>
     <button class="delete" on:click={() => removeTransaction (transaction._id)}></button>
 </div>
